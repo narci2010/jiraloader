c570ba53855816bb48e675521ce435cb40ab4332
cmouilleron <cedric.mouilleron@soprasteria.com>
2016-02-22 17:45:48 +0100

    CMO - Epic is optional for Story and SubTasks

--- JiraLoader/jiraLoader-core/src/main/java/sopra/grenoble/jiraLoader/excel/dto/Story.java
+++ JiraLoader/jiraLoader-core/src/main/java/sopra/grenoble/jiraLoader/excel/dto/Story.java
@@ -8,7 +8,6 @@ public class Story extends GenericModel {
 
 	@Override
 	public boolean validate() {
-		if (epicName == null) return false;
 		if (versionName == null) return false;
 		if (resume == null) return false;
 		if (composantName == null) return false;
